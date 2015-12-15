1e71604a26ee4c13dd68fe4c576252d46c84af23
Abbiii <puzhenliang@gmail.com>
2015-12-14 16:20:02 -0800

    And on his farm he had a cow

--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,15 @@
-class NamedCow
+class NamedCow extends Cow
 {
-
+	private String mooName;
+	public NamedCow()
+	{
+		mooName = "unknown";
+	}
+	public NamedCow(String type, String name, String sound)
+	{
+		myType = type;
+		mooName = name;
+		mySound = sound;
+	}
+	public String getName() {return mooName;}
 }
